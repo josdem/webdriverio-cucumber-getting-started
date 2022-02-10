d83eb14815d2fc9834b3933c19181aeb9bcb5ed7
josdem <joseluis.delacruz@gmail.com>
2022-02-10 14:28:37 -0500

    Using async methods instead

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -2,14 +2,14 @@ const { Given, When, Then } = require('@cucumber/cucumber')
 
 let title
 
-Given('A webpage as {string}', function (keyword) {
+Given('A webpage as {string}', async (keyword) => {
     browser.url(keyword)
 })
 
-When('I get page title', function () {
+When('I get page title', async () => {
     title = browser.getTitle()
 })
 
-Then('I validate title is {string}', function (title) {
+Then('I validate title is {string}', async (title) => {
     expect(title === "josdem").toBeTruthy()
 })
